4ff67404bfda3c21119987f777e3db6680bd23e8
osingcoin <82925795+osingcoin@users.noreply.github.com>
2021-06-01 16:12:51 +0700

    Update styled.d.ts

--- src/style/styled.d.ts
+++ src/style/styled.d.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { PantherTheme } from 'osingswap-uikit/dist/theme'
+import { PantherTheme } from 'osingswap-uikit/src/theme'
 
 declare module 'styled-components' {
   /* eslint-disable @typescript-eslint/no-empty-interface */
